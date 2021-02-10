cc97890ed01b28fb52dd006021fa5f81dd5f3df2
Bernard Maltais <bernard.maltais@canada.ca>
2021-02-10 16:04:32 +0000

    Update

--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -4,6 +4,6 @@ set -e
 
 # https://docs.microsoft.com/en-us/azure/storage/blobs/versioning-enable?tabs=azure-cli
 
-az storage account blob-service-properties update --resource-group ${RESOURCE_GROUP_NAME} --account-name ${STORAGEACCOUNT_NAME} --enable-versioning
+az storage account blob-service-properties update --resource-group "${RESOURCE_GROUP_NAME}" --account-name "${STORAGEACCOUNT_NAME}" --enable-versioning
 
 echo "Versionning was configured for '${STORAGEACCOUNT_NAME}'"
