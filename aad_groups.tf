599f26bc23dab97950f18f2d324f944826cc2725
Bernard Maltais <bernard.maltais@canada.ca>
2021-03-26 08:13:32 -0400

    Adding L0 group members

--- aad_groups.tf
+++ aad_groups.tf
@@ -19,3 +19,27 @@ module azuread_groups_L0 {
   userDefinedString = each.value.userDefinedString
   owners = each.value.owners
 }
+
+# Adding members to groups
+
+data "azuread_users" "L0_Subscription_Owners" {
+  user_principal_names = var.L0_RBAC.ownerNames
+}
+
+data "azuread_users" "L0_Subscription_Contributors" {
+  user_principal_names = var.L0_RBAC.contributorNames
+}
+
+resource "azuread_group_member" "L0_Subscription_Owners-Members" {
+  for_each = toset(data.azuread_users.L0_Subscription_Owners.object_ids)
+
+  group_object_id  = module.azuread_groups_L0.L0_Subscription_Owners.id
+  member_object_id = each.key
+}
+
+resource "azuread_group_member" "L0_Subscription_Contributors-Members" {
+  for_each = toset(data.azuread_users.L0_Subscription_Contributors.object_ids)
+
+  group_object_id  = module.azuread_groups_L0.L0_Subscription_Contributors.id
+  member_object_id = each.key
+}
